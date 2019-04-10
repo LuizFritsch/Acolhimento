41faeb506bd7397abdae54f5fc96d88cd71d2ccb
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2019-04-10 10:17:32 -0300

    Modificações no dao e no tests
    DAO:
    -Criação dos scripts de inserção de cartaosus, residencia e profissoes
    -Prototipo do metodo de inserir paciente
    Tests:
    -Teste insere cartaosus, inserepaciente

--- src/models/dao/OperacoesBancoDeDadosDAO.java
+++ src/models/dao/OperacoesBancoDeDadosDAO.java
@@ -10,7 +10,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import models.CartaoSUS;
 import models.Paciente;
+import models.Residencia;
 
 /**
  *
@@ -19,7 +21,10 @@ import models.Paciente;
 public class OperacoesBancoDeDadosDAO {
 
     private ConexaoBancoDeDadosDAO conexaoDao;
-    private final String INSERT = "INSERT INTO profissoes(codigo, nome, cbo) VALUES (?,?,?)";
+    private final String INSERTPROFISSOES = "INSERT INTO profissoes(codigo, nome, cbo) VALUES (?,?,?)";
+    private final String INSERTCARTAOSUS = "INSERT INTO cartaosus(codigo, numero, cgs) VALUES (?,?,?)";
+    private final String INSERTRESIDENCIA = "INSERT INTO residencia(codigo, rua, numero, bairro, cidade) VALUES (?,?,?,?,?)";
+    private final String INSERTPACIENTE = "INSERT INTO residencia(codigo, nome, cpf, naturalidade, nomemae, datanascimento) VALUES (?,?,?,?,?,?)";
     //private final String UPDATE = "UPDATE CONTATO SET NOME=?, TELEFONE=?, EMAIL=? WHERE ID=?";
     private final String DELETE = "DELETE FROM profissoes WHERE ID =?";
     //private final String LIST = "SELECT * FROM CONTATO";
@@ -30,7 +35,7 @@ public class OperacoesBancoDeDadosDAO {
     }
 
     public void insertProfissoes(String cbo, String nome) throws SQLException {
-        PreparedStatement comando = this.conexaoDao.pegarConexao().prepareStatement(INSERT);
+        PreparedStatement comando = this.conexaoDao.pegarConexao().prepareStatement(INSERTPROFISSOES);
 
         comando.setString(1, null);
         comando.setString(2, nome);
@@ -48,11 +53,42 @@ public class OperacoesBancoDeDadosDAO {
         return resultado;
     }
 
-    public boolean insertPaciente(Paciente paciente) {
-        if (true) {
-            return true;
+    public boolean insertCartaoSUS(CartaoSUS cartaosus) throws SQLException {
+        PreparedStatement comando = this.conexaoDao.pegarConexao().prepareStatement(INSERTCARTAOSUS);
+
+        comando.setString(1, null);
+        comando.setString(2, cartaosus.getNumeroCartaoSUS());
+        comando.setString(3, cartaosus.getNumeroCartaoSUS());
+
+        return comando.execute();
+    }
+
+    public boolean insertResidencia(Residencia residencia) throws SQLException {
+        PreparedStatement comando = this.conexaoDao.pegarConexao().prepareStatement(INSERTRESIDENCIA);
+
+        comando.setString(1, null);
+        comando.setString(2, residencia.getRua());
+        comando.setString(3, residencia.getNumero());
+        comando.setString(4, residencia.getBairro());
+        comando.setString(5, residencia.getCidade());
+
+        return comando.execute();
+    }
+
+    public boolean insertPaciente(Paciente paciente) throws SQLException, Exception {
+        if (insertCartaoSUS(paciente.getCartaoSUS())) {
+            System.out.println("1");
+            if (insertResidencia(paciente.getResidencia())) {
+                System.out.println("A");
+                return true;
+            } else {
+                System.out.println("B");
+                return false;
+            }
         } else {
+            System.out.println("2");
             return false;
         }
+
     }
 }
--- test/models/dao/OperacoesBancoDeDadosDAOTest.java
+++ test/models/dao/OperacoesBancoDeDadosDAOTest.java
@@ -5,8 +5,14 @@
  */
 package models.dao;
 
+import exceptions.CartaoSUSExceptions;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
+import models.CartaoSUS;
+import models.Paciente;
+import models.Profissao;
+import models.Residencia;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -46,11 +52,29 @@ public class OperacoesBancoDeDadosDAOTest {
     public void testSelectTodasProfissoes() throws Exception {
 
     }
-    
+
     @Test
-    public void test2() throws SQLException, ClassNotFoundException{
+    public void test2() throws SQLException, ClassNotFoundException {
         OperacoesBancoDeDadosDAO op = new OperacoesBancoDeDadosDAO();
         op.insertProfissoes("TESTE", "123");
+    }
+
+    @Test
+    public void test3() throws SQLException, ClassNotFoundException, CartaoSUSExceptions {
+        OperacoesBancoDeDadosDAO op = new OperacoesBancoDeDadosDAO();
+        CartaoSUS cartaosus = new CartaoSUS("465465", "4565465460");
+        assertTrue(op.insertCartaoSUS(cartaosus));
+    }
+
+    @Test
+    public void test4() throws SQLException, ClassNotFoundException, CartaoSUSExceptions, Exception {
+        OperacoesBancoDeDadosDAO op = new OperacoesBancoDeDadosDAO();
+        CartaoSUS cartaosus = new CartaoSUS("909067867879090", "90945430");
+        Residencia residencia = new Residencia("", "", "", "");
+        Profissao profissao = new Profissao("");
+        Date data = new Date();
+        Paciente paciente = new Paciente("", "", "", "", "", residencia, data, profissao, cartaosus);
+        assertTrue(op.insertPaciente(paciente));
     }
 
 }
